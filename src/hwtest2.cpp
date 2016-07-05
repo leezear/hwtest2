183da8018b7f9d7197a17dd624c1e9ef350300f7
leezear <187090216@qq.com>
2016-07-05 19:53:55 +0800

    push

---
+++ src/hwtest2.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : hwtest2.cpp
+// Author      : leezear
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
